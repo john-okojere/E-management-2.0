3a29f505071453456f60eb45dd576e929eddf796
john-okojere <johnokojere08@gmail.com>
2024-12-17 21:12:08 +0100

    modified:   emanagement/__pycache__/settings.cpython-313.pyc 	modified:   emanagement/settings.py

--- emanagement/settings.py
+++ emanagement/settings.py
@@ -94,9 +94,18 @@ WSGI_APPLICATION = 'emanagement.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Elegante',  # Replace with your database name
+        'USER': 'admin',               # Initial Super User
+        'PASSWORD': 'trjc0aEP',        # Initial Password
+        'HOST': 'mysql-188312-0.cloudclusters.net',  # Host
+        'PORT': '10194',               # Port
     }
 }
 
